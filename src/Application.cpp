1a6b9036b2ff1e7c2d70fb866336d3119f1c569a
mehmetalicayhan <mehmetalicyhn@gmail.com>
2020-05-17 01:32:06 +0300

    chore: help message

--- src/Application.cpp
+++ src/Application.cpp
@@ -2,44 +2,49 @@
 #include <iostream>
 #include <FileManager.hpp>
 #include "ProjectBuilder.hpp"
+#include <string>
 
 Application::Application(int argc, char const *argv[]) {
-    std::string programArg = argv[1];
+    std::string programArg;
 
     if (argc < 2) {
-        std::cout << "Usage";
+        ProjectCreator::writeHelpPage();
         return;
-    }
-
-    if (programArg == "create") {
-        projectCreator = new ProjectCreator();
-    } else if (programArg == "add") {
-
-        std::string argument = argv[2];
-        std::string fileName = argv[3];
-
-        if (argument == "-c") {
-            std::cout << "create class";
-
-            FileManager::createFile(FileManager::FileType::SOURCE, fileName);
-            FileManager::createFile(FileManager::FileType::HEADER, fileName);
-        } else if (argument == "-s") {
-            std::cout << "create source :";
-
-            FileManager::createFile(FileManager::FileType::SOURCE, fileName);
-        } else if (argument == "-h") {
-            std::cout << "create header :";
-
-            // TODO .h file include in class
-            FileManager::createFile(FileManager::FileType::HEADER, fileName);
-        }
     } else {
-        projectBuilder = new ProjectBuilder();
-        projectBuilder->runCommands(programArg);
+        programArg = argv[1];
+        if (programArg == "create") {
+            projectCreator = new ProjectCreator();
+        } else if (programArg == "add") {
+
+            std::string argument = argv[2];
+            std::string fileName = argv[3];
+
+            if (argument == "-c") {
+                std::cout << "create class";
+
+                FileManager::createFile(FileManager::FileType::SOURCE, fileName);
+                FileManager::createFile(FileManager::FileType::HEADER, fileName);
+            } else if (argument == "-s") {
+                std::cout << "create source :";
+
+                FileManager::createFile(FileManager::FileType::SOURCE, fileName);
+            } else if (argument == "-h") {
+                std::cout << "create header :";
+
+                // TODO .h file include in class
+                FileManager::createFile(FileManager::FileType::HEADER, fileName);
+            }
+        } else if (programArg == "help") {
+            ProjectCreator::writeHelpPage();
+        } else {
+            projectBuilder = new ProjectBuilder();
+            projectBuilder->runCommands(programArg);
+        }
     }
 }
 
 Application::~Application() {
     delete projectCreator;
+    delete projectBuilder;
     delete this;
 }
